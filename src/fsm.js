afa428b13673116e9e4229a6f872db4f9e336ce3
Дмитрий Гулько < >
2017-02-05 20:38:03 +0300

    finite-state-machine

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,54 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(config == null){
+            throw new Error;
+        }else{
+            this._config = config;
+            this._state = this._config.initial;
+        }
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this._state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this._config.states[state]){
+            this._state = state;
+        }else{
+            throw new Error;
+        }
+    }
+
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if(this._config.states[this._state].transitions[event]){
+            this._state = this._config.states[this._state].transitions[event];
+        }else{
+            throw new Error;
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this._state = this._config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,7 +58,17 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (event == undefined){
+            return Object.keys(this._config.states)
+        }
+        if(this._config.states.transitions[event]){
+
+        }else{
+            var A = [];
+            return A;
+        }
+    }
 
     /**
      * Goes back to previous state.
